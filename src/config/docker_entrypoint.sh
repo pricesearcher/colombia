9bae13bb8985ab5d4cec276eff6aea410958f76a
Stephen Francis <stephen.d.francis@gmail.com>
2019-05-22 11:38:29 +0100

    reintroduce build vars and cache name

--- src/config/docker_entrypoint.sh
+++ src/config/docker_entrypoint.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 
-cd /app
-
-ln -s /build/node_modules node_modules
-
 cat /app/src/config/root_bashrc.sh >> /root/.bashrc
 
 exec $@
--- src/header/Header.tsx
+++ src/header/Header.tsx
@@ -1,5 +1,5 @@
 
-// import "./header.scss";
+import "./l-header.scss";
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 
