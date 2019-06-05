358c900c2be5443b23e5eb1fdcddac2571c4389e
Stephen Francis <stephen.d.francis@gmail.com>
2019-06-05 09:07:17 +0100

    server-side example

--- src/server/local.ts
+++ src/server/local.ts
@@ -1,5 +1,9 @@
 
 import Express from "express";
+import React from "react";
+import { renderToString } from "react-dom/server";
+import Header from "../header/Header";
+import Header2 from "../header/Header2";
 import * as Data from "../data/linked_product_advert.json";
 
 const app = Express();
@@ -11,6 +15,18 @@ app.get("/random", (req: Express.Request, res: Express.Response) => {
     "Content-Type": "text/plain"
   });
   res.end(String(Math.ceil(Math.random() * 1000 * Data.body.approximate_result_count)));
+});
+
+app.get("/ssr", (req: Express.Request, res: Express.Response) => {
+  const props = {};
+  const jsx: React.ComponentElement<any, any> = (Math.random() < 0.5)
+    ? React.createElement(Header , props)
+    : React.createElement(Header2, props);
+
+  res.writeHead(200, {
+    "Content-Type": "text/html"
+  });
+  res.end(renderToString(jsx));
 });
 
 app.listen(8081);
